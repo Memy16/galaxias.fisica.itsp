3b98650460ff3bb1de992b9f563dc7e53e708fae
emmy <machadodeoliveirairigoitaemmy@gmail.com>
2024-03-19 17:34:12 -0300

    Nav funcional, datos, bibliografia, ico y estilos

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let navbarToggler = document.querySelector('.navbar-toggler');
+let navbarCollapse = document.querySelector('.navbar-collapse');
+    navbarToggler.addEventListener('click', function () {
+    navbarCollapse.classList.toggle('show');
+});
