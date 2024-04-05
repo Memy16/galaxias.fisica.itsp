8caf619122aacaafcfae4dd4e06ae5358e3edc6d
emmy <machadodeoliveirairigoitaemmy@gmail.com>
2024-04-05 03:19:00 -0300

    comentarios hechos con padlet

--- js/app.js
+++ js/app.js
@@ -16,36 +16,3 @@ const firebaseConfig = {
     appId: "1:294199813129:web:25501ac87d5428cd5d44d8",
     measurementId: "G-JWTBGMN90S",
 };
-const app = firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-        // Escuchar el evento submit del formulario
-        const commentForm = document.getElementById('commentForm');
-        commentForm.addEventListener('submit', (e) => {
-            e.preventDefault();
-            const name = document.getElementById('nameInput').value;
-            const comment = document.getElementById('commentInput').value;
-            // Guardar el comentario en la base de datos de Firebase
-            database.ref('comments').push({
-                name: name,
-                comment: comment
-            });
-            // Limpiar los campos de nombre y comentario
-            document.getElementById('nameInput').value = '';
-            document.getElementById('commentInput').value = '';
-        });
-
-        // Mostrar los comentarios en cards
-        const commentsContainer = document.getElementById('commentsContainer');
-        database.ref('comments').on('child_added', (snapshot) => {
-            const comment = snapshot.val();
-            const card = document.createElement('div');
-            card.className = 'card';
-            card.innerHTML = `
-                <div class="card-body">
-                    <h5 class="card-title">${comment.name}</h5>
-                    <p class="card-text">${comment.comment}</p>
-                </div>
-            `;
-            commentsContainer.appendChild(card);
-            console.log('Comentario agregado:', comment);
-        });
