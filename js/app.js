0e96813051b2f927ad1a37dc959a2f8863800e81
emmy <machadodeoliveirairigoitaemmy@gmail.com>
2024-04-05 15:42:58 -0300

    referencia comentario agregada

--- js/app.js
+++ js/app.js
@@ -4,15 +4,3 @@ let navbarCollapse = document.querySelector('.navbar-collapse');
     navbarCollapse.classList.toggle('show');
 });
 
-import firebase from 'firebase/app';
-import 'firebase/database';
-
-const firebaseConfig = {
-    apiKey: "AIzaSyB14k1XdEoqzLzRE_KY4xNB-L2ededePQc",
-    authDomain: "galaxias---2do-bc---fisica.firebaseapp.com",
-    projectId: "galaxias---2do-bc---fisica",
-    storageBucket: "galaxias---2do-bc---fisica.appspot.com",
-    messagingSenderId: "294199813129",
-    appId: "1:294199813129:web:25501ac87d5428cd5d44d8",
-    measurementId: "G-JWTBGMN90S",
-};
